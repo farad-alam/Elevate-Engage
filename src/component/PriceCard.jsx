61e5d9761f03eff257ae0878a9a61e44b4a9ef7f
Farad Alam <farad.dev@gmail.com>
2025-03-05 16:34:29 +0600

    button hange

--- src/component/PriceCard.jsx
+++ src/component/PriceCard.jsx
@@ -61,7 +61,7 @@ function PriceCard({
 
           <a
             href="#"
-            className="block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+            className="block rounded-lg  px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 main-pricing-btn focus-visible:ring  md:text-base"
           >
             {buttonTxt}
           </a>
